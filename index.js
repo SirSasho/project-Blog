71ccb3d11768d8aec2adbf9d0789ce11ccb89a68
SirSasho <73391613+SirSasho@users.noreply.github.com>
2022-01-02 17:27:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const currentPage = window.location.pathname.replace("/", "").replace(".html", "")
+
 if (localStorage.getItem("blogRegister") == "" || localStorage.getItem("blogRegister") == null) {
     localStorage.setItem("blogRegister", JSON.stringify([]))
 }
 const register = JSON.parse(localStorage.getItem("blogRegister"));
+
 if (localStorage.getItem("blogPosts") == "" || localStorage.getItem("blogPosts") == null) {
     localStorage.setItem("blogPosts", JSON.stringify([]))
 }
 const blogPosts = JSON.parse(localStorage.getItem("blogPosts"));
+const blogLogin = JSON.parse(localStorage.getItem("blogLogin"));
+
+
+
+function isLogged() {
+    if (blogLogin) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 if (currentPage == "login") {
     const submitReg = document.getElementById("login-submit");
+    const creatPost = document.getElementById("post");
+    const logOut = document.querySelector(".logOut");
 
     submitReg.addEventListener("click", event => {
         const email = document.getElementById("login-email").value;
@@ -22,6 +37,8 @@ if (currentPage == "login") {
                 if (password == userFromRegister.password) {
                     // email, e syshtoto kato email: email, 
                     localStorage.setItem("blogLogin", JSON.stringify({ email, password }));
+                    alert("sucsessful login!");
+                    location.href = `/index.html`;
                 } else {
                     alert("Wrong Password")
                 }
@@ -48,6 +65,7 @@ if (currentPage == "login") {
                     localStorage.setItem("blogRegister", JSON.stringify(register))
                     console.log(register)
                     alert("Registered!")
+                    location.href = `/index.html`;
                 }
             }
         } else {
@@ -60,7 +78,7 @@ if (currentPage == "login") {
     } else {
         const submit = document.getElementById("submit-post");
         submit.addEventListener("click", event => {
-            const titleValue = document.getElementById("title").value;
+            const titleValue = document.getElementById("title-post").value;
             const postValue = document.getElementById("post").value;
             const currentUser = JSON.parse(localStorage.getItem("blogLogin"));
             const postData = {
@@ -77,44 +95,70 @@ if (currentPage == "login") {
         });
     }
 } else if (currentPage == "index") {
+    const posts = document.querySelector(".div-post");
     const listPost = document.getElementById("list");
+    const logOut = document.querySelector(".logOut");
+    const creatPost = document.getElementById("creat-post");
+    const logIn = document.getElementById("log-in");
+
+    logIn.addEventListener("click", event => {
+        location.href = `/login.html`;
+    });
+
     if (window.location.search == "") {
         listPost.innerHTML = ""
         blogPosts.forEach(({ title, comments, date }) => {
             const d = new Date(date)
             listPost.innerHTML += `
-            <li><a href="/index.html?title=${title}">
-                <h2>${title}</h2>
-                <h3>From: ${d.getDate()}/${(d.getMonth()+1)}/${d.getFullYear()}</h3>
-                <h4> Comment Number: ${comments.length}</h4>
-            </a></li>`
+                <li><a href="/index.html?title=${title}">
+                    <h3>${title}</h3>
+                    <p>From: ${d.getDate()}/${(d.getMonth()+1)}/${d.getFullYear()}</p>
+                    <p> Comment Number: ${comments.length}</p>
+                </a></li>`
         });
     } else {
+        document.getElementById("back-main").removeAttribute("hidden");
+
         const searchTitle = window.location.search.replace("?title=", "")
         const searchPost = blogPosts.find(({ title }) => title == searchTitle)
+
         if (searchTitle) {
             const { title, comments, date, user, post } = searchPost;
             const d = new Date(date)
             let c = comments.reduce((total, { cUser, cText, cDate }) => {
                 const cd = new Date(cDate)
                 return `${total}<div>
-                    <h2>${cUser}</h2>
-                    <h3>${cd.getDate()}/${(cd.getMonth()+1)}/${cd.getFullYear()}</h3>
-                    <h4>${cText}</h4>
-                </div>`
+                        <h2>${cUser}</h2>
+                        <h3>${cd.getDate()}/${(cd.getMonth()+1)}/${cd.getFullYear()}</h3>
+                        <h4>${cText}</h4>
+                    </div>`
             }, "")
             if (c == "") {
                 c = "No Comments"
             }
-            listPost.innerHTML = `<li>
-                <h2>${title}</h2>
-                <h2>${user}</h2>
-                <h3>From: ${d.getDate()}/${(d.getMonth()+1)}/${d.getFullYear()}</h3>
-                <h4>Post: ${post}</h4>
-                <div> Comments: ${c}</div>
-            </li>`
+            listPost.innerHTML = `
+                    <h2>${title}</h2>
+                    <h2>${user}</h2>
+                    <h3>From: ${d.getDate()}/${(d.getMonth()+1)}/${d.getFullYear()}</h3>
+                    <h4>Post: ${post}</h4>
+                    <div> Comments: ${c}</div>
+                `
+
         } else {
             alert("No Post Found!")
         }
+    }
+
+    if (isLogged()) {
+        logOut.removeAttribute("hidden");
+        creatPost.removeAttribute("hidden");
+        logOut.addEventListener("click", event => {
+            localStorage.setItem("blogLogin", JSON.stringify(""));
+            alert("click")
+            location.href = `/index.html`;
+        })
+        console.log(isLogged())
+    } else {
+        logIn.removeAttribute("hidden")
     }
 }
